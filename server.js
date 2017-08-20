b93d06873fea4a19ea2141aa1bb39cfdd4ad5b7a
mdevaki554 <manikanta.gupta554@gmail.com>
2017-08-21 00:40:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,32 +25,39 @@ var articleOne = {
 			</div>`
 };
 
-var htmlTemplate = `<html>
-	<head>
-		<title> ${title} </title>
-		<meta name = "viewport" content = "width-device-width, initial-scale-1" />
-		<link href= "/ui/style.css" rel = "stylesheet"/>
-	</head>
-	<body>
-	    <div class = 'container'>
-    		<div>
-    			<a href="/">Home</a>
-    		</div>
-    		<hr>
-    		<div>
-    			${heading}
-    		</div>
-    		
-    		<div>
-    			${date}
-    		</div>
-    		<div>
-    			${content}
-    		</div>
-    	  </div>
-		</body>
-	</body>
-</html>`;
+funtions createTemplate(data){
+    var title = data.title;
+    var date = data.date
+    var heading = data.heading
+    var content = data.content;
+    var htmlTemplate = `<html>
+    	<head>
+    		<title> ${title} </title>
+    		<meta name = "viewport" content = "width-device-width, initial-scale-1" />
+    		<link href= "/ui/style.css" rel = "stylesheet"/>
+    	</head>
+    	<body>
+    	    <div class = 'container'>
+        		<div>
+        			<a href="/">Home</a>
+        		</div>
+        		<hr>
+        		<div>
+        			${heading}
+        		</div>
+        		
+        		<div>
+        			${date}
+        		</div>
+        		<div>
+        			${content}
+        		</div>
+        	  </div>
+    		</body>
+    	</body>
+    </html>`;
+    return htmlTemplate; 
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
