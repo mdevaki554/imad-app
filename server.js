d65daab08d9c09b2f8ee9fd782f24ee704c0aa7f
mdevaki554 <manikanta.gupta554@gmail.com>
2017-08-21 00:49:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var articleOne = {
 funtions createTemplate(data){
     var title = data.title;
     var date = data.date;
-    var heading = data.heading
+    var heading = data.heading;
     var content = data.content;
     var htmlTemplate = `<html>
     	<head>
