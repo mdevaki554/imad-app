34404025a5c664197d7cdd51b4ff2cc6aaa38f51
mdevaki554 <manikanta.gupta554@gmail.com>
2017-08-21 01:33:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var mainText = document.getElementById("main-text");
 mainText.innerHTML = 'New Value';
+
+var madi = document.getElementById('madi');
+madi.onClick = function() {
+madi.style.marginLeft = '100px';
+};
