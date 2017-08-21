b07e670dd01077b50b3060043e73961addc0f337
mdevaki554 <manikanta.gupta554@gmail.com>
2017-08-21 19:55:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,13 @@
 //Counter Example
-var counter_button = document.getElementById('counter')
+var counter_button = document.getElementById('counter');
+var counter = 0;
 counter_button.onClick = function( ){
   //Making req to End point '/Counter'
   
   //capturing th output from the end point
   
   //rendering the output on count 
+  counter = counter +1 ;
   var span_count = document.getElementById('count');
   span_count.innerHTML = counter.toString();
 };
