895db1ac92710085070b59cb0b6eb1e134fcb727
mdevaki554 <manikanta.gupta554@gmail.com>
2017-08-21 20:13:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ counter_button.onclick = function(){
   //capturing th output from the end point
   request.onreadystatechange = function(){
     if (request.readyState ===   XMLHttpRequest.DONE) {
-        if(reques.status === 200 ){
+        if(request.status === 200 ){
         var counter = request.responseText;
         var span_count = document.getElementById('count');
         span_count.innerHTML = counter.toString();
